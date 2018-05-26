5162d025e92ddc46221d90eed1c821a3d2385018
Maria Gl <nachtmarie.photo@gmail.com>
2018-05-26 16:57:45 +0300

    search

--- blogbody/views.py
+++ blogbody/views.py
@@ -38,3 +38,37 @@ def archieve(request):
     except EmptyPage:
         posts = paginator.page(paginator.page_num)
     return render(request, 'archieve.html', {'page': page, 'posts': posts})
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+def get_query(query_string, search_fields):
+    query = None 
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+
+def search(request):
+    query_string = ''
+    found_entries = None
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+
+        entry_query = get_query(query_string, ['title', 'text',])
+
+        found_entries = Post.objects.filter(entry_query).order_by('-published_date')
+
+    return render(request, 'search.html', { 'query_string': query_string, 'found_entries': found_entries })
